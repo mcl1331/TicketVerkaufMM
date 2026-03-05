12d2377708c0ee996a03c91b62358a155dd9b070
Sandra Gangelmaier <sandragangelmaier15@gmail.com>
2026-03-05 14:12:39 +0100

    Initial commit

---
+++ TiketverkaufMM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiketverkaufMM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

7b5b66f9b1db8f9a73a86bad6e5efb5ff1f1c84b
Sandra Gangelmaier <sandragangelmaier15@gmail.com>
2026-03-10 12:02:35 +0100

    Create reservierung frontend working version

--- TiketverkaufMM/Models/ErrorViewModel.cs
+++ TiketverkaufMM/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-//namespace TicketverkaufMM.Models
-//{
-//    public class ErrorViewModel
-//    {
-//        public string? RequestId { get; set; }
+namespace TicketverkaufMM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-//        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-//    }
-//}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

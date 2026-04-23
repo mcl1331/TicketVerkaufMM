c98f57d418e44b6a262e4e226b26628f13914e6c
mcl1331 <meichia.lin.23@gmail.com>
2026-04-23 09:38:34 +0200

    Create Account & Login

---
+++ TicketverkaufMM/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+// hinzufuegen
+using TicketverkaufMM.ViewModels;
+
+namespace TicketverkaufMM.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    public AccountController(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Login Versuch
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Ungültiger Login-Versuch");
+        }
+        // Bei Fehler view neu anzeigen (Validation Summary zeigt Fehler)
+        return View(model);
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    // Einfache AccessDenied Anzeige
+    public IActionResult AccessDenied()
+    {
+        return Content("Zugriff verweigert. Notwendige Rechte fehlen");
+    }
+}

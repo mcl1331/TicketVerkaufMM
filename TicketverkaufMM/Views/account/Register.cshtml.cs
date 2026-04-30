bcb463ba3b0ed5026057e04df83c5b92cd3eae9d
Sandra Gangelmaier <sandragangelmaier15@gmail.com>
2026-04-30 11:20:25 +0200

    Closes #15 - Add Register View and RegisterViewModel

---
+++ TicketverkaufMM/Views/account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TicketverkaufMM.Views.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
